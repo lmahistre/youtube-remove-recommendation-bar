d31f9aee87c4e82a5d521e57957d449758852c05
lmahistre <lionel.mahistre@gmail.com>
2020-07-15 11:21:55 +0200

    update id of DOM element

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.1 (15/07/2020)
+
+* Update id of DOM element
--- yrrb.js
+++ yrrb.js
@@ -1,1 +1,6 @@
-document.getElementById('chips-below').remove();
+if (document.getElementById('chips-below')) {
+	document.getElementById('chips-below').remove();
+}
+if (document.getElementById('chips')) {
+	document.getElementById('chips').remove();
+}
